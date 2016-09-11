483b078ca46a1c053963da1c99bf03fc41da32da
Eliran Haziza <eliranhaze@gmail.com>
2016-09-12 00:54:37 +0300

    added equivalence classes to graph

--- main.py
+++ main.py
@@ -7,16 +7,35 @@ import expressions
 
 def make_relations(expressions, e, relation):
     mapping = {}
+    equivs = []
     i = 0
     for e1 in expressions:
         i += 1
         print '(%d/%d) %s' % (i, len(expressions), e1)
         for e2 in expressions:
             if e1 != e2 and not is_trans(e1, e2, mapping):
-                if relation(e1, e2, e) and not relation(e2, e1, e):
-                    eset = mapping.setdefault(e1, set())
-                    eset.add(e2)
-    return mapping
+                if relation(e1, e2, e):
+                    other_direction = relation(e2, e1, e)
+                    if other_direction:
+                        found = False
+                        for eq in equivs:
+                            if e1 in eq or e2 in eq:
+                                eq.add(e1)
+                                eq.add(e2)
+                                found = True
+                                break
+                        if not found:
+                            equivs.append({e1, e2})
+                    else:
+                        eset = mapping.setdefault(e1, set())
+                        eset.add(e2)
+#    equivs = []
+#    for e1 in expressions:
+#        for e2 in expressions:
+#            if e1 != e2 and relation(e1, e2, e) and relation(e2, e1, e):
+#                if {e1, e2} not in equivs:
+#                    equivs.append({e1,e2})
+    return mapping, equivs
 
 def is_trans(e1, e2, mapping, indirect=False):
     if not e1 in mapping:
@@ -39,7 +58,8 @@ def main():
     all_exps = exp.expressions
 
     print 'mapping'
-    mapping = make_relations(all_exps, e, relation=exp.closer_to_than)
+    mapping, equivs = make_relations(all_exps, e, relation=exp.closer_to_than)
+    print 'equivs', equivs
 
     print 'filtering'
     for f, f_list in mapping.iteritems():
@@ -48,14 +68,21 @@ def main():
     print 'writing output:', outfile
     # PASTE OUTPUT TO: http://www.webgraphviz.com/
     with open(outfile, 'w') as out:
-        out.write("digraph G {\n")
-        out.write("rankdir=BT;\n")
-        out.write("size=\"100,100\";\n")
-        out.write("node [shape = box];\n")
+        out.write('digraph G {\n')
+        out.write('  rankdir=BT;\n')
+        out.write('  size="100,100";\n')
+        out.write('  node [shape = box];\n')
+        for i, equiv in enumerate(equivs):
+            out.write('  subgraph cluster_%d {\n' % i)
+            out.write('    color=blue;\n')
+            for eq in equiv:
+                out.write('    "%s";\n' % eq)
+            out.write('  }\n')
         for f, f_list in mapping.iteritems():
           for f_l in f_list:
-            out.write('"%s" -> "%s"\n' % (f, f_l))
-        out.write("}\n")
+            out.write('  "%s" -> "%s"\n' % (f, f_l))
+        out.write('}\n')
+
     print 'done'
 
 ###################################################################################################
